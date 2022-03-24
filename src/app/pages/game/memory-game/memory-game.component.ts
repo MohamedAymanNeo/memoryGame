828145f2b36e10e8b036d3cd6ddbe31d52d9f10c
Mohamed.Ayman <Mohamed.Ayman@P9W1-501>
2022-03-24 16:50:24 +0200

    push

--- src/app/pages/game/memory-game/memory-game.component.ts
+++ src/app/pages/game/memory-game/memory-game.component.ts
@@ -9,42 +9,43 @@ export class MemoryGameComponent implements OnInit {
   @ViewChildren("ref", { read: ElementRef })
   cardRef!: QueryList<ElementRef<HTMLParagraphElement>>;
   type: any;
-  x:any = []; // first card index
-  y:any = []; // second card index
-  m:any = 0;
-  i:any = 0;
-  gameStart:boolean = false;
-  shuffleNumber:any = 0;
-  ele:any;
-  moves:any = 0;
-  oneTimeIf:any = true;
-  timeNow:any;
-  clearInt:any;
-  seconds:any;
-  minutes:any;
-  progress:any = 0;
-  time:any;
-  socialIcons:any =[];
-  numbersIcons:any =[];
-  FlagsIcons:any =[];
-  animalsIcons:any =[
-    {type: 'social', icons: ['facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github', 'facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github']},
-    {type: 'numbers', icons: ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']}
+  x: any = []; // first card index
+  y: any = []; // second card index
+  m: any = 0;
+  i: any = 0;
+  gameStart: boolean = true;
+  shuffleNumber: any = 0;
+  ele: any;
+  moves: any = 0;
+  oneTimeIf: any = true;
+  timeNow: any;
+  clearInt: any;
+  seconds: any;
+  minutes: any;
+  progress: any = 0;
+  time: any;
+  socialIcons: any = [];
+  numbersIcons: any = [];
+  FlagsIcons: any = [];
+  firstRef: any;
+  animalsIcons: any = [
+    { type: 'social', icons: ['facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github', 'facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github'] },
+    { type: 'numbers', icons: ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'] }
   ];
-  src:any;
+  src: any;
   // socialIcons = ['facebook-square', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github', 'facebook-square', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github'];
-  icon:any =[]
+  icon: any = []
   icons: any;
-  realArray:any = [];
-  constructor(private route: ActivatedRoute) { 
-    this.route.params.subscribe((res:any) => {
+  realArray: any = [];
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe((res: any) => {
       this.type = res['slug'];
 
-      this.shuffleIcons('',this.type)
+      this.shuffleIcons('', this.type)
       // this.chooseType(this.type);
     });
-    
-    
+
+
   }
 
   ngAfterViewInit(): void {
@@ -52,9 +53,9 @@ export class MemoryGameComponent implements OnInit {
   }
 
   ngOnInit(): void {
-        
+
   }
-  chooseType(type:any) {
+  chooseType(type: any) {
     switch (type) {
       case 'social':
 
@@ -67,162 +68,152 @@ export class MemoryGameComponent implements OnInit {
         // this.realArray = this.numbersIcons;
         // this.shuffle(this.realArray,type)
         break;
-    
+
       default:
         break;
     }
   }
-  checkItem(item: any) {
-    // if(!this.gameStart) {
-    //   this.gameStart =true;
-    //   this.startGame(item)
-    // } 
-
-  }
   startTimer() {
     // ---- Time counter ----
-    if (this.oneTimeIf) {
-      
-      this.oneTimeIf = false;
-      this.timeNow = new Date().getTime();
-  
-      this.clearInt = setInterval( () => {
-  
-        this.time = new Date().getTime() - this.timeNow;
-        this.seconds = Math.floor((this.time /1000) % 60);
-        this.minutes = Math.floor((this.time /1000) / 60);
-  
-        if (this.seconds < 10) {
-          $('.seconds').text('0' + this.seconds);
-        } else {
-          $('.seconds').text(this.seconds);
-        }
-  
-        if (this.minutes < 10) {
-          $('.minutes').text('0' + this.minutes);
-        } else {
-          $('.minutes').text(this.minutes);
-        }
-  
-      }, 1000);
-      
-      $('.info').fadeIn();
-    }
+    this.timeNow = new Date().getTime();
+
+    this.clearInt = setInterval(() => {
+
+      this.time = new Date().getTime() - this.timeNow;
+      this.seconds = Math.floor((this.time / 1000) % 60);
+      this.minutes = Math.floor((this.time / 1000) / 60);
+
+      if (this.seconds < 10) {
+        $('.seconds').text('0' + this.seconds);
+      } else {
+        $('.seconds').text(this.seconds);
+      }
+
+      if (this.minutes < 10) {
+        $('.minutes').text('0' + this.minutes);
+      } else {
+        $('.minutes').text(this.minutes);
+      }
+
+    }, 1000);
+
+    $('.info').fadeIn();
   }
-  startGame(item: any,myRef: any) {
-    this.cardRef;
-    if(this.gameStart) {
-      this.gameStart =true;
-      this.startTimer()
-    } 
-
-    // perform only on not solved cards or not showed cards
-    if (! $(this).hasClass('succes') && ! $(this).hasClass('show')) { 
-  
-      this.ele = $('.card').index($(this)); // checkeing depend on fliped ele index
-      console.log(this.ele);
-  
-      if (this.i == 0) { // pushing first card index
-        this.x.push(this.ele);
-        this.i = 1;
-      } else { // pushing second card index
-        this.y.push(this.ele); 
-        this.i = 0;
-      }
-  
-      $(this).removeClass('flip').addClass('show'); // show card
-      console.log(this.x, this.y, 'checking index is ' + this.m + ' now');
-  
-      if (this.x.length == this.y.length ) { // start comparing
-  
-        this.moves += 1; // for moves counter
-        $('.moves').text(this.moves); // diplay moves in Document
-  
-        // this time out makes transition complete befor adding or remove classes
-        // and solve quick clicking bugs
-        setTimeout( ()=> { 
-  
-          // start checking depend on m variable thats get the index of pushed cards
-          // console.log(x, y) in the top explain this
-          if ($('.card').eq(this.x[this.m]).data('checked') == $('.card').eq(this.y[this.m]).data('checked')) {
-            // console.log($('.card').eq(this.x[this.m]).data('checked'), $('.card').eq(this.y[this.m]).data('checked'));
-            $('.card').eq(this.x[this.m]).removeClass('show').addClass('succes');
-            $('.card').eq(this.y[this.m]).removeClass('show').addClass('succes');
-            this.m += 1;
-            // --------- progress for displaying win message ---------
-            this.progress += 2;
-  
-          } else {
-  
-            $('.card').eq(this.x[this.m]).removeClass('show').addClass('flip');
-            $('.card').eq(this.y[this.m]).removeClass('show').addClass('flip');
-            this.m += 1;
-  
-          }
-  
-          if (this.progress == 16) {
-            
-            clearInterval(this.clearInt); // stop Time Counter
-            setTimeout(function () {
-              $('.info').fadeOut();
-              $('.result').fadeIn(1000);
-            }, 700);
-            
-          }
-        }, 700)
-      }
-    }
-    // $('.wrapper').on('click', '.card', function () {
-
+  secondClick: any = 1;
+  startGame(item?: any, myRef?: any, myIndex?: any) {
+    console.log(this.secondClick);
     
-    // });
+    if (this.secondClick < 3) {
+      this.secondClick++;
+      if (this.oneTimeIf) {
+        console.log(this.oneTimeIf);
+        this.oneTimeIf = false;
+        this.startTimer()
+      }
+
+      // perform only on not solved cards or not showed cards
+      if (!myRef.classList.contains('succes', 'show')) {
+        this.ele = myIndex; // checkeing depend on fliped ele index
+        // console.log("this.ele");
+
+        if (this.i == 0) { // pushing first card index
+          this.x.push(this.ele);
+          // -------------Check this ref not equal it self
+          this.firstRef = myRef;
+          this.i = 1;
+        } else { // pushing second card index
+          this.y.push(this.ele);
+          this.i = 0;
+        }
+        myRef.classList.remove('flip');
+        myRef.classList.add('show'); // show card  
+        if (this.x.length == this.y.length) { // start comparing
+          this.moves += 1; // for moves counter
+          $('.moves').text(this.moves); // diplay moves in Document
+          // this time out makes transition complete befor adding or remove classes
+          // and solve quick clicking bugs
+          setTimeout(() => {
+            // start checking depend on m variable thats get the index of pushed cards
+            // console.log(x, y) in the top explain this
+
+            // -----------check if this ref not equal it self
+            // &&  myRef.id != myRef.id
+            if (myRef.id == this.firstRef.id ) {
+              myRef.classList.remove('show')
+              myRef.classList.add('succes')
+              this.firstRef.classList.remove('show')
+              this.firstRef.classList.add('succes')
+              this.m += 1;
+              // --------- progress for displaying win message ---------
+              this.progress += 2;
+            } else {
+              myRef.classList.remove('show')
+              myRef.classList.add('flip')
+              this.firstRef.classList.remove('show')
+              this.firstRef.classList.add('flip')
+
+              this.m += 1;
+            }
+            if (this.progress == 16) {
+
+              clearInterval(this.clearInt); // stop Time Counter
+              setTimeout(function () {
+                $('.info').fadeOut();
+                $('.result').fadeIn(1000);
+              }, 700);
+
+            }
+          }, 700)
+        }
+      }
+    }
+    if (this.secondClick == 3) {
+      this.secondClick --
+      return
+    }
   }
 
-  shuffleIcons(passedArr?:any, type?: any) {       // shuffling depends on splice 'removing array items'
-    this.animalsIcons.map((item: any,index:any) => {
-      if(item.type == type) {
+  shuffleIcons(passedArr?: any, type?: any) {   // shuffling
+    this.animalsIcons.map((item: any, index: any) => {
+      if (item.type == type) {
         this.realArray = item.icons.sort(() => Math.random() - 0.5)
         console.log(this.realArray);
+
       }
-      
     })
-    // return Math.floor(Math.random() * this.realArray.length);
-    // debugger;
-    // this.icons= icons;
-    // for (;;) {
-    //   if (this.icons.length === 0) {break;}
-    //   var n = Math.floor(Math.random() * this.icons.length);
-    //   // console.log(n);
-    //   $('.wrapper').append('<div class="card flip" data-checked="' + this.icons[n] + '"><img class="img" src=""assets/images/"'+ type+'/'+this.icons[n]+'"/></div>')
-    //   this.icons.splice(n, 1);
-    // }
   }
+  checkClicks() {
+    this.cardRef.map((item) => {
+      console.log(item);
 
+      // item.nativeElement.onclick((it) => {
+      //   console.log(it);
+
+      // })
+    })
+  }
   // ---------------------------- Restart --------------------
-// all about resetting
-restart() {
-  // $('.card').each(function () {
-  //   $(this).remove();
-  // });
-  this.icons = this.icon.slice(0);
-  // this.shuffle();
+  // all about resetting
+  restart() {
+    this.shuffleIcons('', this.type);
+    // this.startGame()
+    $('.info').hide();
+    this.oneTimeIf = true;
+    this.firstRef = null;
+    this.moves = 0;
+    this.progress = 0;
+    this.cardRef.map((item) => {
+      console.log(item);
+      item.nativeElement.classList.remove('succes');
+      item.nativeElement.classList.add('flip');
+    })
+    this.m = 0;
+    clearInterval(this.clearInt);
 
-  $('.info').hide();
-  
-  this.oneTimeIf = true;
-  this.moves = 0;
-  this.progress = 0;
-  this.x.splice(0);
-  this.y.splice(0);
-  this.m = 0;
-  clearInterval(this.clearInt);
-  
-  $('.result').fadeOut(1000);
-  $('.minutes').text('00');
-  $('.seconds').text('00');
-  $('.moves').text('0');
-  // $('.restart').click(function () {
-  // });
-}
+    $('.result').fadeOut(1000);
+    $('.minutes').text('00');
+    $('.seconds').text('00');
+    $('.moves').text('0');
+  }
 
 }
