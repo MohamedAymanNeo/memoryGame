995316a328f4b51c2c44511cc32839ee13612a1a
MohamedAymanNeo <az.mohamedayman12@gmail.com>
2022-03-27 07:37:38 +0200

    push

--- src/app/pages/game/memory-game/memory-game.component.ts
+++ src/app/pages/game/memory-game/memory-game.component.ts
@@ -55,24 +55,7 @@ export class MemoryGameComponent implements OnInit {
   ngOnInit(): void {
 
   }
-  chooseType(type: any) {
-    switch (type) {
-      case 'social':
-
-        // this.socialIcons = ['facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github', 'facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github'];
-        // this.realArray = this.socialIcons;
-        // this.shuffle(this.realArray,type)
-        break;
-      case 'numbers':
-        // this.numbersIcons = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
-        // this.realArray = this.numbersIcons;
-        // this.shuffle(this.realArray,type)
-        break;
-
-      default:
-        break;
-    }
-  }
+ 
   startTimer() {
     // ---- Time counter ----
     this.timeNow = new Date().getTime();
@@ -99,12 +82,15 @@ export class MemoryGameComponent implements OnInit {
 
     $('.info').fadeIn();
   }
-  secondClick: any = 1;
+  secondClick: any = 0;
   startGame(item?: any, myRef?: any, myIndex?: any) {
-    console.log(this.secondClick);
+    this.secondClick++;
+    this.checkClicks(this.secondClick, myRef)
+    // -------------Check this ref not equal it self
     
-    if (this.secondClick < 3) {
-      this.secondClick++;
+    // this.checkClicks(this.secondClick)
+    if (this.secondClick < 2) {
+      console.log("after",this.secondClick);
       if (this.oneTimeIf) {
         console.log(this.oneTimeIf);
         this.oneTimeIf = false;
@@ -118,79 +104,102 @@ export class MemoryGameComponent implements OnInit {
 
         if (this.i == 0) { // pushing first card index
           this.x.push(this.ele);
-          // -------------Check this ref not equal it self
-          this.firstRef = myRef;
           this.i = 1;
         } else { // pushing second card index
           this.y.push(this.ele);
           this.i = 0;
         }
-        myRef.classList.remove('flip');
-        myRef.classList.add('show'); // show card  
         if (this.x.length == this.y.length) { // start comparing
           this.moves += 1; // for moves counter
           $('.moves').text(this.moves); // diplay moves in Document
-          // this time out makes transition complete befor adding or remove classes
-          // and solve quick clicking bugs
-          setTimeout(() => {
-            // start checking depend on m variable thats get the index of pushed cards
-            // console.log(x, y) in the top explain this
-
-            // -----------check if this ref not equal it self
-            // &&  myRef.id != myRef.id
-            if (myRef.id == this.firstRef.id ) {
-              myRef.classList.remove('show')
-              myRef.classList.add('succes')
-              this.firstRef.classList.remove('show')
-              this.firstRef.classList.add('succes')
-              this.m += 1;
-              // --------- progress for displaying win message ---------
-              this.progress += 2;
-            } else {
-              myRef.classList.remove('show')
-              myRef.classList.add('flip')
-              this.firstRef.classList.remove('show')
-              this.firstRef.classList.add('flip')
-
-              this.m += 1;
-            }
-            if (this.progress == 16) {
-
-              clearInterval(this.clearInt); // stop Time Counter
-              setTimeout(function () {
-                $('.info').fadeOut();
-                $('.result').fadeIn(1000);
-              }, 700);
-
-            }
-          }, 700)
+          // this.successOrNot(myRef)
         }
       }
     }
-    if (this.secondClick == 3) {
-      this.secondClick --
-      return
-    }
   }
 
-  shuffleIcons(passedArr?: any, type?: any) {   // shuffling
-    this.animalsIcons.map((item: any, index: any) => {
-      if (item.type == type) {
-        this.realArray = item.icons.sort(() => Math.random() - 0.5)
-        console.log(this.realArray);
+  
+  checkClicks(clicksNumber: any, checkRef?:any) {
+    debugger;
+    console.log(checkRef);
+    console.log(this.firstRef);
+
+    switch (clicksNumber) {
+      case 0:
+        
+        break;
+      case 1:
+        this.addShowRemoveFlip(checkRef)
+        this.firstRef = checkRef;
+        break;
+        case 2:
+          if(this.firstRef != checkRef) {
+            checkRef.classList.add('show');
+            checkRef.classList.remove('flip'); // flip card if user click twice
+            this.firstRef.classList.add('flip'); // flip card if user click twice
+            this.firstRef.classList.remove('show');
+            this.secondClick = 0;  
+          } else {
+          // this.successOrNot(checkRef);
+          checkRef.classList.remove('show');
+          checkRef.classList.add('flip'); // flip card if user click twice
+          this.secondClick = 0; 
+        }
+        console.log('hi');
+        break;
+    
+      default:
+        break;
+    }
+    // this.cardRef.map((item) => {
+    //   console.log(item);
+
+    //   // item.nativeElement.onclick((it) => {
+    //   //   console.log(it);
+
+    //   // })
+    // })
+  }
+  addShowRemoveFlip(showCard:any) {
+    showCard.classList.remove('flip');
+    showCard.classList.add('show'); // show card  
+  }
+  successOrNot(successRef:any) {
+    debugger;
+    // this time out makes transition complete befor adding or remove classes
+    // and solve quick clicking bugs
+    setTimeout(() => {
+      // start checking depend on m variable thats get the index of pushed cards
+      // console.log(x, y) in the top explain this
+
+      // -----------check if this ref not equal it self
+      // &&  myRef.id != myRef.id
+      if (successRef.id == this.firstRef.id ) {
+        successRef.classList.remove('show')
+        successRef.classList.add('succes')
+        this.firstRef.classList.remove('show')
+        this.firstRef.classList.add('succes')
+        this.m += 1;
+        // --------- progress for displaying win message ---------
+        this.progress += 2;
+      } else {
+        successRef.classList.remove('show')
+        successRef.classList.add('flip')
+        this.firstRef.classList.remove('show')
+        this.firstRef.classList.add('flip')
+
+        this.m += 1;
+      }
+      if (this.progress == 16) {
+
+        clearInterval(this.clearInt); // stop Time Counter
+        setTimeout(function () {
+          $('.info').fadeOut();
+          $('.result').fadeIn(1000);
+        }, 700);
 
       }
-    })
-  }
-  checkClicks() {
-    this.cardRef.map((item) => {
-      console.log(item);
-
-      // item.nativeElement.onclick((it) => {
-      //   console.log(it);
-
-      // })
-    })
+    }, 700)
   }
   // ---------------------------- Restart --------------------
   // all about resetting
@@ -215,5 +224,32 @@ export class MemoryGameComponent implements OnInit {
     $('.seconds').text('00');
     $('.moves').text('0');
   }
+  shuffleIcons(passedArr?: any, type?: any) {   // shuffling
+    this.animalsIcons.map((item: any, index: any) => {
+      if (item.type == type) {
+        this.realArray = item.icons.sort(() => Math.random() - 0.5)
+        console.log(this.realArray);
 
+      }
+    })
+  }
+
+  chooseType(type: any) {
+    switch (type) {
+      case 'social':
+
+        // this.socialIcons = ['facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github', 'facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github'];
+        // this.realArray = this.socialIcons;
+        // this.shuffle(this.realArray,type)
+        break;
+      case 'numbers':
+        // this.numbersIcons = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+        // this.realArray = this.numbersIcons;
+        // this.shuffle(this.realArray,type)
+        break;
+
+      default:
+        break;
+    }
+  }
 }
