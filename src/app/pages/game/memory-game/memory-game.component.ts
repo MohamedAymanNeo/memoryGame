bcd07a10cb53a06421e947bdc3ac68c65f933698
MohamedAymanNeo <az.mohamedayman12@gmail.com>
2022-03-28 23:42:59 +0200

    push updates

--- src/app/pages/game/memory-game/memory-game.component.ts
+++ src/app/pages/game/memory-game/memory-game.component.ts
@@ -28,6 +28,9 @@ export class MemoryGameComponent implements OnInit {
   numbersIcons: any = [];
   FlagsIcons: any = [];
   firstRef: any;
+  firstIndex: any;
+  firstCardName: any;
+  firstClick: boolean = true;
   animalsIcons: any = [
     { type: 'social', icons: ['facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github', 'facebook', 'youtube', 'instagram', 'codepen', 'linkedin', 'twitter', 'twitch', 'github'] },
     { type: 'numbers', icons: ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'] }
@@ -82,124 +85,114 @@ export class MemoryGameComponent implements OnInit {
 
     $('.info').fadeIn();
   }
-  secondClick: any = 0;
-  startGame(item?: any, myRef?: any, myIndex?: any) {
-    this.secondClick++;
-    this.checkClicks(this.secondClick, myRef)
-    // -------------Check this ref not equal it self
+  checkCard(cardName:any, cardRef:any, cardIndex:any) {
+    // debugger
+    console.log(this.firstClick);
     
-    // this.checkClicks(this.secondClick)
-    if (this.secondClick < 2) {
-      console.log("after",this.secondClick);
-      if (this.oneTimeIf) {
-        console.log(this.oneTimeIf);
-        this.oneTimeIf = false;
-        this.startTimer()
-      }
+    if(this.firstClick) {
+      this.firstRef = cardRef;
+      this.firstIndex = cardIndex;
+      this.firstCardName = cardName;
+      this.firstClick = false;
+      cardRef.classList.remove('flip');
+      cardRef.classList.add('show'); // show card
+    } else {
+      cardRef.classList.remove('flip');
+      cardRef.classList.add('show'); // show card
 
-      // perform only on not solved cards or not showed cards
-      if (!myRef.classList.contains('succes', 'show')) {
-        this.ele = myIndex; // checkeing depend on fliped ele index
-        // console.log("this.ele");
-
-        if (this.i == 0) { // pushing first card index
-          this.x.push(this.ele);
-          this.i = 1;
-        } else { // pushing second card index
-          this.y.push(this.ele);
-          this.i = 0;
-        }
-        if (this.x.length == this.y.length) { // start comparing
-          this.moves += 1; // for moves counter
-          $('.moves').text(this.moves); // diplay moves in Document
-          // this.successOrNot(myRef)
-        }
-      }
+      setTimeout(() => {
+        this.checkSuccess(this.firstRef,cardRef,this.firstCardName,cardName);
+      }, 700);
     }
   }
-
+  checkSuccess(firstCard?:any, secondCard?: any,firstCardName?:any, secondCardName?:any ) {
+    // debugger;
+    console.log('---------------------');
+    
+    console.log(firstCard);
+    console.log(secondCard);
+    console.log(firstCardName);
+    console.log(secondCardName);
+    if(firstCardName != secondCardName) {
+      firstCard.classList.remove('show');
+      firstCard.classList.add('flip'); // hide card
+      secondCard.classList.remove('show');
+      secondCard.classList.add('flip'); // hide card
+    } else {
+      firstCard.classList.remove('show')
+      firstCard.classList.add('succes')
+      secondCard.classList.remove('show')
+      secondCard.classList.add('succes')
+      this.m += 1;
+      // --------- progress for displaying win message ---------
+      this.progress += 2;
+    }
+  }
   
-  checkClicks(clicksNumber: any, checkRef?:any) {
-    debugger;
-    console.log(checkRef);
-    console.log(this.firstRef);
-
-    switch (clicksNumber) {
-      case 0:
-        
-        break;
-      case 1:
-        this.addShowRemoveFlip(checkRef)
-        this.firstRef = checkRef;
-        break;
-        case 2:
-          if(this.firstRef != checkRef) {
-            checkRef.classList.add('show');
-            checkRef.classList.remove('flip'); // flip card if user click twice
-            this.firstRef.classList.add('flip'); // flip card if user click twice
-            this.firstRef.classList.remove('show');
-            this.secondClick = 0;  
-          } else {
-          // this.successOrNot(checkRef);
-          checkRef.classList.remove('show');
-          checkRef.classList.add('flip'); // flip card if user click twice
-          this.secondClick = 0; 
-        }
-        console.log('hi');
-        break;
-    
-      default:
-        break;
+  // secondClick: any = 0;
+  startGame(item?: any, myRef?: any, myIndex?: any) {
+    if(this.oneTimeIf) {
+      this.oneTimeIf = false
+      this.startTimer()
     }
-    // this.cardRef.map((item) => {
-    //   console.log(item);
-
-    //   // item.nativeElement.onclick((it) => {
-    //   //   console.log(it);
-
-    //   // })
-    // })
-  }
-  addShowRemoveFlip(showCard:any) {
-    showCard.classList.remove('flip');
-    showCard.classList.add('show'); // show card  
-  }
-  successOrNot(successRef:any) {
-    debugger;
-    // this time out makes transition complete befor adding or remove classes
-    // and solve quick clicking bugs
-    setTimeout(() => {
-      // start checking depend on m variable thats get the index of pushed cards
-      // console.log(x, y) in the top explain this
-
-      // -----------check if this ref not equal it self
-      // &&  myRef.id != myRef.id
-      if (successRef.id == this.firstRef.id ) {
-        successRef.classList.remove('show')
-        successRef.classList.add('succes')
-        this.firstRef.classList.remove('show')
-        this.firstRef.classList.add('succes')
-        this.m += 1;
-        // --------- progress for displaying win message ---------
-        this.progress += 2;
-      } else {
-        successRef.classList.remove('show')
-        successRef.classList.add('flip')
-        this.firstRef.classList.remove('show')
-        this.firstRef.classList.add('flip')
-
-        this.m += 1;
+    // perform only on not solved cards or not showed cards
+    if (!myRef.classList.contains('succes', 'show')) {
+      // console.log('hi');
+      this.ele = myIndex; // checkeing depend on fliped ele index
+      console.log(this.ele);
+      
+      if (this.i == 0) { // pushing first card index
+        this.x.push(this.ele);
+        this.i = 1;
+      } else { // pushing second card index
+        this.y.push(this.ele); 
+        this.i = 0;
       }
-      if (this.progress == 16) {
+      eval(myRef).classList.remove('flip')
+      eval(myRef).classList.add('show')
+      eval(myRef).setAttribute("data-checked", item );
+      if(this.x.length == this.y.length) { // start comparing
+        this.moves += 1; // for moves counter
+        $('.moves').text(this.moves); // diplay moves in Document
+        // this time out makes transition complete befor adding or remove classes
+        // and solve quick clicking bugs
+        
+        // console.log(document.querySelectorAll(`#${item}`)[0].getAttribute("data-checked"));
+        console.log($('.card').eq(this.x[this.m]).data('checked'));
+        console.log($('.card').eq(this.y[this.m]).data('checked'));
+        
+        setTimeout(() => {
+          if(this.ele != this.ele) {
+            
+          }
+          if ($('.card').eq(this.x[this.m]).data('checked') == $('.card').eq(this.y[this.m]).data('checked')) {
+            console.log("-------",$('.card').eq(this.x[this.m]).data('checked'), $('.card').eq(this.y[this.m]).data('checked'));
+            $('.card').eq(this.x[this.m]).removeClass('show').addClass('succes');
+            $('.card').eq(this.y[this.m]).removeClass('show').addClass('succes');
+            this.m += 1;
+            // --------- progress for displaying win message ---------
+            this.progress += 2;
+  
+          } else {
+  
+            $('.card').eq(this.x[this.m]).removeClass('show').addClass('flip');
+            $('.card').eq(this.y[this.m]).removeClass('show').addClass('flip');
+            this.m += 1;
+  
+          }
+          if (this.progress == 16) {
+          
+            clearInterval(this.clearInt); // stop Time Counter
+            setTimeout(function () {
+              $('.info').fadeOut();
+              $('.result').fadeIn(1000);
+            }, 700);
+            
+          }
 
-        clearInterval(this.clearInt); // stop Time Counter
-        setTimeout(function () {
-          $('.info').fadeOut();
-          $('.result').fadeIn(1000);
         }, 700);
-
       }
-    }, 700)
+    }
   }
   // ---------------------------- Restart --------------------
   // all about resetting
